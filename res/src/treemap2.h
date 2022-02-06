723e4e6d52e789bee0ea0f4fd59e47926f3a12d4
esotericsean <sean@seanduran.com>
2022-02-05 19:44:06 -0800

    Add files via upload

---
+++ res/src/treemap2.h
@@ -0,0 +1,8 @@
+#ifndef MAP_treemap2_H
+#define MAP_treemap2_H
+#define treemap2Width 20
+#define treemap2Height 18
+#include "MapInfo.h"
+extern unsigned char bank_treemap2;
+extern struct MapInfo treemap2;
+#endif
