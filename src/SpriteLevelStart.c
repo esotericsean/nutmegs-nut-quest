4c6ec8544879ef41e071306b27e7099bae8d9b7d
ShenMansellPersonal <shenmansell@gmail.com>
2024-10-05 21:26:48 +1300

    add back in "Level Start" blinking

--- src/SpriteLevelStart.c
+++ src/SpriteLevelStart.c
@@ -7,6 +7,10 @@
 
 #include "../src/GlobalVars.h"
 
+// move the level start sprite offscreeen to make it "blink"
+// save the old y pos so we can move it back
+static UINT16 oldY;
+
 void Start_SpriteLevelStart(void) 
 {
     THIS->custom_data[0] = 0;
@@ -14,11 +18,30 @@ void Start_SpriteLevelStart(void)
 
 void Update_SpriteLevelStart(void) 
 {
-    THIS->custom_data[0] ++;
-    if (THIS->custom_data[0] == 101)
+    
+    if (THIS->custom_data[0] == 0)
+    {
+        oldY = THIS->y;
+        THIS-> y = (UINT16) -20;
+    }
+    else if (THIS->custom_data[0] == 10)
+    {
+        THIS->y = oldY;
+    }
+    else if (THIS->custom_data[0] == 40)
+    {
+        THIS-> y = (UINT16) -20;
+    }
+    else if (THIS->custom_data[0] == 70)
+    {
+        THIS->y = oldY;
+    }
+    else if (THIS->custom_data[0] == 100)
     {
         SpriteManagerRemoveSprite(THIS);
     }
+
+    THIS->custom_data[0] ++;
 }
 
 void Destroy_SpriteLevelStart(void) 
